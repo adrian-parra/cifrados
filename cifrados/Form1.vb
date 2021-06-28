f14b7efa616320755bdf3dbd4945abf0275f03f0
adrian <adrian.adrianborquez@gmail.com>
2021-06-27 21:36:30 -0600

    p

--- cifrados/Form1.vb
+++ cifrados/Form1.vb
@@ -1,5 +1,7 @@
 ﻿Public Class Form1
-    Dim Tsalida, Tentrada As New ArrayList
+    Dim Tsalida, Tentrada, Vtabla, Valeatorio, valcifrado As New ArrayList
+
+    Dim m As String = ""
 
     Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
 
@@ -30,7 +32,7 @@
     'FUNCION PARA GENERAR TABLA
     Private Function GenerarTabla(k As Integer)
         Dim valFin As String = Str(2 ^ k)
-        MsgBox(valFin)
+
 
 
         For i = 0 To CInt(valFin)
@@ -54,6 +56,104 @@
 
     End Function
 
+    'FUNCIO PARA CONVERTIR VALOR A BINARIO
+    Private Function ConvBin(val As String) As String
+        Dim b As String = ""
+        Dim baux As String = ""
+        Do Until val < 2
+            b = b + Str(val Mod 2)
+            val = Int(val / 2)
+        Loop
+
+        b = b + val
+
+        b = StrReverse(b)
+        For i = 1 To b.Length
+            If Mid(b, i, 1) <> " " Then
+                baux += Mid(b, i, 1)
+            End If
+        Next
+        Return baux
+    End Function
+
+    'FUNCION PARA AGREGAR 0 LEFT
+    Private Function addCero(val As String, k As String) As String
+        Dim addc As Integer = 0
+        Dim addValCeros As String = ""
+
+
+        If val.Length < Int(k) Then
+            addc = Int(k) - val.Length
+
+            'add 0
+            For i = 1 To addc
+                addValCeros += "0"
+            Next
+
+
+        End If
+        Return addValCeros + val
+    End Function
+
+    'BUTTON PARA CIFRAR
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+
+        'Mi parte 
+        If cb_metodocifrado.Text = "Encadenamiento de bloques (i num aleatorios)" Then
+
+            If txt_textoclaro.Text.Length = 1 Then
+                m = ConvBin(Str(Asc(txt_textoclaro.Text)))
+                m = addCero(m, "9")
+
+                'generar num random
+                For i = 0 To CInt(txt_k.Text) - 1
+                    Valeatorio.Add(addCero(ConvBin(Str(NumR(txt_k.Text))), txt_k.Text))
+                    'MsgBox(Valeatorio(i))
+
+                Next
+
+                'agrupar mensaje en tres
+                Dim contPareja As Integer = 0
+                Dim val As String = ""
+                For i = 1 To m.Length
+                    contPareja += 1
+                    val += Mid(m, i, 1)
+
+                    MsgBox("val  " + val)
+                    MsgBox("cont " + Str(contPareja))
+                    If contPareja = 3 Then
+
+                        'buscar valor en la tabla
+                        For j = 0 To Tentrada.Count - 1
+                            If val = Tentrada(j) Then
+                                Vtabla.Add(Tsalida(j))
+                                MsgBox(Tsalida(j))
+                                Exit For
+                            End If
+                        Next
+
+                        contPareja = 0
+                        val = ""
+                    End If
+                Next
+
+            End If
+            'su parte 
+        ElseIf cb_metodocifrado.Text = "Encadenamiento de bloquez (1 num aleatorio)" Then
+
+
+        End If
+    End Sub
+
+    'FUNCION PARA GENERAR NUMEROS RANDOM
+    Private Function NumR(k As String) As Integer
+        Dim v As Integer = (2 ^ CInt(k))
+
+
+        Return CInt(Rnd() * v)
+
+    End Function
+
     'BUTTON PARA GENERAR TABLA
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
         'VERIFICAR LOS TXT
@@ -65,7 +165,7 @@
 
             'MOSTRAR TABLA
             For i = 0 To Tsalida.Count - 1
-                lv_metodo.Items.Add("entrada " + Tentrada(i) + " --> salida " + Tsalida(i))
+                lv_metodo.Items.Add("entrada " + addCero(ConvBin((Tentrada(i))), txt_k.Text) + " --> salida " + addCero(ConvBin(Tsalida(i)), txt_k.Text))
             Next
         End If
 
